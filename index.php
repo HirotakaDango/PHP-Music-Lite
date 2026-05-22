1fcf49b3e25430f4685ccb443aa1758c69a5d9d3
赤葦だんご <104591072+HirotakaDango@users.noreply.github.com>
2026-05-22 23:27:15 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ if (isset($_GET['action']) && $_GET['action'] === 'get_songs') {
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Simple Music Player</title>
+    <title>PHP-Music-Lite</title>
     <style>
       body {
         font-family: sans-serif;
